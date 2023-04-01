d2fafd23242fcd48d52e4548bd4d6a1fa5b2f801
Xylot <Joseph.meriam@gmail.com>
2023-03-31 23:02:07 -0500

    Rename tmux.conf to .tmux.conf and add docker -> podman alias

--- lib/config/tmux.zsh
+++ lib/config/tmux.zsh
@@ -1,3 +1,3 @@
-export TMUX_CONFIG="${TMUX_CONFIG:-$XDG_CONFIG_HOME/tmux/tmux.conf}"
-[[ -f $TMUX_CONFIG ]] || { mkdir -p $TMUX_CONFIG:h && touch $TMUX_CONFIG }
+export TMUX_CONFIG="${TMUX_CONFIG:-$XDG_CONFIG_HOME/tmux/.tmux.conf}"
+[[ -e $TMUX_CONFIG ]] || { mkdir -p $TMUX_CONFIG:h && touch $TMUX_CONFIG }
 alias tmux='tmux -f "$TMUX_CONFIG"'
