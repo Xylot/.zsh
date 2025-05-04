0990d22cd5090a3461737878d152492d1a4901eb
Xylot <Joseph.meriam@gmail.com>
2025-05-04 05:58:20 -0500

    Fix fzf config

--- lib/config/fzf.zsh
+++ lib/config/fzf.zsh
@@ -1,3 +1,10 @@
+FZF_DIR=$XDG_DATA_HOME/fzf
+
+if [[ ! -f $XDG_CONFIG_HOME/fzf/fzf.zsh ]]; then
+    [[ -e $FZF_DIR ]] || git clone --quiet --depth=1 https://github.com/junegunn/fzf $FZF_DIR
+    $FZF_DIR/install --xdg --key-bindings --completion --no-update-rc --no-bash --no-fish
+fi
+
 if [[ -f $XDG_CONFIG_HOME/fzf/fzf.zsh ]]; then
     source $XDG_CONFIG_HOME/fzf/fzf.zsh
 fi
